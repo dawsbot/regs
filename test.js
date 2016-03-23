f4e4ed83598ec326d24c6c66c265cf4e42b4bf1f
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2016-03-22 18:50:11 -0600

    add windows build

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ import test from 'ava';
 import regs from './';
 import toType from 'to-type';
 
-test('title', t => {
+test('yeoman', t => {
   t.is(toType(regs.yeoman()), 'regexp');
-  t.is(regs.yeoman().exec('<% var1 %>')[1], 'var1');
-  t.is(regs.yeoman().exec('<%= var1 %>')[1], 'var1');
+  t.is(regs.yeoman().exec('<% var1a %>')[1], 'var1a');
+  t.is(regs.yeoman().exec('<%= var1b %>')[1], 'var1b');
 
   const myReg = regs.yeoman('g');
-  t.is(myReg.exec('<% var1 %><% var2 %>')[1], 'var1');
-  t.is(myReg.exec('<% var1 %><% var2 %>')[1], 'var2');
+  t.is(myReg.exec('<% var2a %><% var2b %>')[1], 'var2a');
+  t.is(myReg.exec('<% var2a %><% var2b %>')[1], 'var2b');
 });
